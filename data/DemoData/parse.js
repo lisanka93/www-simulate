5d9e039e9cfd473c70b99366465fbe1edeed277f
FilWisher <williamfisher@hotmail.co.uk>
2016-05-11 21:54:22 +0100

    cleans data

--- data/DemoData/parse.js
+++ data/DemoData/parse.js
@@ -11,6 +11,7 @@ fs.readdirSync('./data/DemoData')
     var data = require(path.resolve(__dirname, f))
 
     var obj = {}
+
     obj.events = data['EVENTS'].map(function (e) {
     
         e.type = e.event_type
@@ -54,47 +55,3 @@ fs.readdirSync('./data/DemoData')
    // })
 })
 
-/*
-var data = require(path)
-
-var obj = {}
-obj.events = data['EVENTS'].map(function (e) {
-
-    e.type = e.event_type
-    return e
-})
-obj.topology = {}
-
-//console.log(data['TOPOLOGY']['NODES'])
-
-function find_node_index(n) {
-    for (var i = 0; i < obj.topology['nodes'].length; i++) {
-        if (obj.topology['nodes'][i].name == n) {
-            return i
-        }
-    }
-}
-
-obj.topology['nodes'] = Object.keys(data['TOPOLOGY']['NODES']).map(function (n) {
-
-    return {
-        name: n,
-        type: data['TOPOLOGY']['NODES'][n].stack[0] 
-    }
-})
-
-obj.topology['edges'] = Object.keys(data['TOPOLOGY']['EDGES']).map(function (n) {
-
-    return {
-        source: find_node_index(data['TOPOLOGY']['EDGES'][n][0]),
-        target: find_node_index(data['TOPOLOGY']['EDGES'][n][1]),
-        value: data['TOPOLOGY']['EDGES'][n][2].weight
-    }
-
-})
-
-//console.log(obj.topology.edges)
-
-//var data = require('../../data/demo.json')
-console.log(JSON.stringify(obj))
-*/
